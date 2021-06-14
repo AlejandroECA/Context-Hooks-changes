4c6b0a833ee1f787993f8f534fb7f8534218c0fc
Alejandro Carrillo <alejandroecarrilloa@gmail.com>
2021-06-14 13:51:22 -0500

    first commit

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React,{useContext} from 'react';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
-import { selectCollection } from '../../redux/shop/shop.selectors';
-
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
+import CollectionContext from '../../contexts/collections/collections.context'
+
+const CollectionPage = ({ match }) => {
+
+  const collections = useContext(CollectionContext)
+  const collection = collections[match.params.collectionId]
+
+
   const { title, items } = collection;
+
+
   return (
+    
     <div className='collection-page'>
       <h2 className='title'>{title}</h2>
       <div className='items'>
@@ -21,8 +28,5 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
 
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage
